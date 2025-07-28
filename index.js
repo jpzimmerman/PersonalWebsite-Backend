b26144f889350b5a6c80f537c2d65fa5120a0da0
Joshua Zimmerman <jpz6926@gmail.com>
2025-07-27 20:50:55 -0400

    Feature update: added PATCH endpoint for submitting blog post reacts. Endpoint receives an object Id (submitted to frontend via the getBlogPosts endpoint), and increments the 'like' count for a post with a given id

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 import express from "express";
 import cors from "cors";
-import { MongoClient } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 
 const port = process.env.port || 2727;
 
 const app = express();
 const mongoDbClient = new MongoClient("mongodb://localhost:27017");
+const pwblogCollection = "pwblog";
 
-app.use(cors());
+const corsOptions = {
+  origin: "*", // Replace with your frontend domain
+  methods: "*",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: [
+    "Access-Control-Allow-Methods",
+    "Access-Control-Allow-Origin",
+    "Content-Type",
+    "Authorization",
+  ],
+};
+
+app.use(cors(corsOptions));
 
 let conn;
 conn = await mongoDbClient.connect();
@@ -20,8 +34,21 @@ app.listen(port, () => {
   console.log("Personal website backend, listening on PORT:", port);
 });
 
+app.options("/blogPosts/:postId/addLike", cors(corsOptions));
+
 app.get("/getBlogPosts", async (request, response) => {
-  let collection = await db.collection("pwblog");
+  let collection = await db.collection(pwblogCollection);
   let results = await collection.find({}).toArray();
+  response.send(JSON.stringify(results));
+});
+
+app.patch("/blogPosts/:postId/addLike", async (request, response) => {
+  let postId = request.params.postId;
+  let collection = await db.collection(pwblogCollection);
+  console.log(postId);
+  let results = await collection.updateOne(
+    { _id: new ObjectId(postId) },
+    { $inc: { numberOfLikes: 1 } }
+  );
   response.send(JSON.stringify(results));
 });
